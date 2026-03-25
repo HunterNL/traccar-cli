2414fb80fb2d19ede1a15d4f26898fe606d8a6fb
Hunter <HunterNL@users.noreply.github.com>
2026-03-25 16:53:13 +0100

    Add session_get

--- src/session.rs
+++ src/session.rs
@@ -1,11 +1,24 @@
+use serde_json::Value;
+
 use crate::Traccar;
 
-pub struct SessionResponse {
-    id: u32,
-    name: String,
-    email: String
-}
+// pub struct SessionResponse {
+//     id: u32,
+//     name: String,
+//     email: String,
+// }
 
 impl Traccar {
-    async fn session_get(&self) -> SessionResponse
+    pub async fn session_get(&self) -> Option<Value> {
+        let path = "/api/session";
+        let path = self.host.clone().join(path).unwrap();
+
+        let req = self.http_client.get(path);
+        let req = req.query(&[("token", self.token.clone())]);
+
+        match req.send().await {
+            Ok(r) => r.json().await.ok(),
+            Err(_) => None,
+        }
+    }
 }
